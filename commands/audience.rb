09b2a21366fd1b7820635ce4db6dd3fa1fdd8af6
Kang-Kyu Lee <kangkyu1111@gmail.com>
2016-10-14 16:38:05 -0700

    Send email to the slack user asked audit

--- commands/audience.rb
+++ commands/audience.rb
@@ -2,21 +2,21 @@ module Vicki
   module Commands
     class Audience < SlackRubyBot::Commands::Base
       command 'audience' do |client, data, _match|
-        uri = URI('http://localhost:3000/audits')
+        uri = URI('http://localhost:3000/api/audits')
 
         req = Net::HTTP::Post.new(uri)
-        req.set_form_data("youtube_url" => "#{_match[:expression]}")
+        args = _match[:expression].split(' ')
+        req.set_form_data("youtube_url" => args.first, 'email' => "#{args.last}@fullscreen.net")
 
         res = Net::HTTP.start(uri.hostname, uri.port) do |http|
           http.request(req)
         end
         case res
         when Net::HTTPSuccess, Net::HTTPRedirection
-          client.say channel: data.channel, text: JSON.parse(res.body)['url']
+          client.say channel: data.channel, text: "I'm crunching the numbers now, you will get an email soon."
         else
           client.say channel: data.channel, text: "Ooops"
         end
-        client.say channel: data.channel, text: "hello Philip"
       end
     end
   end
